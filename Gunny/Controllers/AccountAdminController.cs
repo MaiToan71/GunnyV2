217c245a916985a8cf60ed6f75ffc46cf0984d9d
iammaitoan <maitoan97hd@gmail.com>
2022-04-30 17:18:32 +0700

    Add project files.

---
+++ Gunny/Controllers/AccountAdminController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gunny.Controllers
+{
+    public class AccountAdminController : Controller
+    {
+        [Route("/ca-nhan")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
